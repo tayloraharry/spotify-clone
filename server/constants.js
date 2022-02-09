7189cec11e33dc19c4a139229edfad53b73e723b
Taylor Harry <tharry@trinisys.loc>
2022-02-09 12:55:41 -0600

    removed secrets

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
-const clientId = "c37775c135a2430dab5191206bc5c2b4";
-const clientSecret = "1b7811ebf62843cd96cbf46fc7516817";
+const clientId = "";
+const clientSecret = "";
 
 exports.clientId = clientId;
 exports.clientSecret = clientSecret;
